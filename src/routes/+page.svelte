12fdb70edaefd66b22a5a8c76287f45aa773a8be
MAttila42 <menyhart.atika@gmail.com>
2024-07-31 15:35:03 +0200

    ✨ feat: GitHub login

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,26 @@
 <script lang='ts'>
+  import { createClient } from '@supabase/supabase-js'
   import Component from '$lib/components/Component.svelte'
   import { Button } from '$lib/components/ui/button'
 
+  const supabase = createClient('https://bdsmazevljfrfeneymcl.supabase.co', 'public-anon-key')
+
   let count = $state(0)
+
+  async function signInWithGithub() {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: 'github',
+    })
+    if (error)
+      console.error('GitHub OAuth error:', error)
+  }
 </script>
+
+<Button
+  onclick={() => signInWithGithub()}
+>
+  Login with GitHub
+</Button>
 
 <Button
   onclick={() => count += 1}
